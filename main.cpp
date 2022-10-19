3fd3068f5a083546031beb3256a0895f25881a5b
Rafou98 <95037521+Rafou28@users.noreply.github.com>
2022-10-19 16:22:17 +0200

    Colored PLAYERS

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
 #include <iomanip>
+#include <windows.h>
+
+
 
 using namespace std;
 
 const int ROWS = 6;
 const int COLS = 7;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
 enum CELL {
     BLANK, PLAYER1, PLAYER2
@@ -23,7 +27,7 @@ int main() {
 
     do {
         int input;
-
+        SetConsoleTextAttribute(hConsole, 7);
         cout << "Enter a number between 1 and " << COLS << " : ";
         cin >> input;
         cout << " " << endl;
@@ -34,8 +38,11 @@ int main() {
             //cout << " " << endl;
             cerr << "Entree invalide " << endl;
             continue;
-
         }
+
+
+
+
     } while (!isOver);
 
 
@@ -43,20 +50,32 @@ int main() {
 }
 
 string getPlayerName(CELL cell) {
+
+
     switch (cell) {
         case 0:
+            SetConsoleTextAttribute(hConsole, 7);
             return "0";
+
         case 1:
+
+            SetConsoleTextAttribute(hConsole, 14);
             return "PLAYER1";
+
         case 2:
+            SetConsoleTextAttribute(hConsole, 10);
             return "PLAYER2";
+
     }
+
 }
 
 void showGrid() {
+
     for (int y = 0; y < ROWS; ++y) {
         for (int x = 0; x < COLS; ++x) {
-            cout << setw(7) << getPlayerName(CELL(grid[x][y])) << " ";
+
+            cout << setw(7)  << getPlayerName(CELL(grid[x][y])) << " ";
         }
         cout << endl;
     }
